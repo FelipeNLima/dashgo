6e7d13f06afce4512b604be439eaa2afc4f08ac1
Felipe Lima <felipinho.lima27@gmail.com>
2022-04-21 19:52:59 -0300

    feat: iniciando o projeto com next e utilizando chakra.ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import { Input } from '../components/Form/Input'
+
+const SignIn: NextPage = () => {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="Email" />
+          <Input name="password" type="password" label="password" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default SignIn
